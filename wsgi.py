237084ac452a84dfeeb276fe2f34fd242d1aef49
llenax <67596732+llenax@users.noreply.github.com>
2021-03-28 16:29:25 +0300

    s

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
 import os
-from dotenv import load_dotenv
-from app.main import app
+import discord
+from discord.ext import commands
 
-load_dotenv()
 
 if __name__ == "__main__":
-  app.run()
+    load_dotenv()
+
+    client = commands.AutoShardedBot(shard_count=1, command_prefix="!", intents=discord.Intents.all())
+
+    @client.event
+    async def on_ready():
+        print('Ready!')
+
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
+
+    client.run(os.getenv("DISCORD_TOKEN"))
